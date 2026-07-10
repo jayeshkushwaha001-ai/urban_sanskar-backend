b33a601f080c84a2f16a9ea223809cd78c7b2b9c
jayeshkushwaha001-ai <jayeshkushwaha001@gmail.com>
2026-07-10 23:12:14 +0530

    Add 'stories' category to product schema

--- models/Product.js
+++ models/Product.js
@@ -26,7 +26,7 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Product category is required'],
         enum: {
-            values: ['coord-sets', 'dress', 'handloom-dupatta' ,'hand-series'],
+            values: ['coord-sets', 'dress', 'handloom-dupatta' ,'hand-series', 'stories'],
             message: '{VALUE} is an invalid category'
         }
     },
